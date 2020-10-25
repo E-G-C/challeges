c34eb1a63af3ac3a9a91e60032203d2b6e58a20b
EG <9368226+E-G-C@users.noreply.github.com>
2020-10-25 16:07:24 -0400

    codility challenge

---
+++ day_of_the_week.py
@@ -0,0 +1,21 @@
+# having the days of the week in the form of
+# "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"
+# given a string s indicating a day of the week and a number k
+# return what day of the week will be k days
+# example s="Sat" k = 9 should return "Mon"
+# example s="Mon" k = 3 should return "Thu"
+
+def solution(s, k):
+
+    days_of_the_week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    week_length = 7
+    current_index = days_of_the_week.index(s)
+    remaining = (k + current_index) % week_length
+    return days_of_the_week[remaining]
+
+
+assert solution("Sat", 9) == "Mon"
+assert solution("Wed", 210) == "Wed"
+assert solution("Thu", 2) == "Sat"
+
+print("Ok")
